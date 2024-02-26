78e53f7af5dbc21e4c05b23e0fb6d3b21b9c5898
Harinder <harindersingh2107@gmail.com>
2024-02-26 17:25:26 +0000

    added tooltip on card

--- src/components/Card/AlbumCard.jsx
+++ src/components/Card/AlbumCard.jsx
@@ -7,21 +7,24 @@ import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
+import Tooltip from "@mui/material/Tooltip";
 
 const AlbumCard = ({ data,type}) => {
   return (
-    <Paper sx={{ backgroundColor: "#121212", padding: 1, color: "#FFFFFF" }} elevation={0}>
-      <Card sx={{ borderRadius: "10px" }}>
-        <CardMedia component="img" height={200} image={data.image} alt={data.title} />
+    <Tooltip title={type!="song"?`${data.songs?.length} Songs`:''} placement="top" >
+      <Paper sx={{ backgroundColor: "#121212", padding: 1, color: "#FFFFFF" }} elevation={0}>
+        <Card sx={{ borderRadius: "10px" }}>
+          <CardMedia component="img" height={200} image={data.image} alt={data.title} />
 
-        <CardActionArea>
-          <Chip label={type==="song"?`${data.likes} Likes`:`${data.follows} Follows`} sx={{ marginTop: 1, marginBottom: 1, marginLeft: 1, backgroundColor: "#121212", color: "#FFFFFF" }} />
-        </CardActionArea>
-      </Card>
-      <Typography variant="subtitle1" component="div">
-        {data.title}
-      </Typography>
-    </Paper>
+          <CardActionArea>
+            <Chip label={type==="song"?`${data.likes} Likes`:`${data.follows} Follows`} sx={{ marginTop: 1, marginBottom: 1, marginLeft: 1, backgroundColor: "#121212", color: "#FFFFFF" }} />
+          </CardActionArea>
+        </Card>
+        <Typography variant="subtitle1" component="div">
+          {data.title}
+        </Typography>
+      </Paper>
+    </Tooltip>
   );
 };
 
